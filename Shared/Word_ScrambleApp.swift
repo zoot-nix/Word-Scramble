aad1dde02ab145d59ea61ea00718f9174a12b53e
Owais Shaikh <theowaiszoot@gmail.com>
2022-07-04 16:27:02 +0530

    Initial Commit

---
+++ Shared/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Shared
+//
+//  Created by Owais Shaikh on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
